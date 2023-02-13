b65ebf90a3015e6f90c033f375be140747a71653
Andrew Vreeland <andrewvreeland@gmail.com>
2023-02-13 17:46:55 -0500

    ACP proccess inorder to create new working branch

---
+++ app.js
@@ -0,0 +1,33 @@
+// TODO: dynamically generate salmon objects using form data
+
+'use strict';
+
+
+// ****************** HELPER FUNCTION **************
+
+function randomCook(min, max) {
+  Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomCust(min, max) {
+  Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomCookSale() {
+  let cookiesale = 1
+  for (i = 0; i < arrOfHours.length; i++) {
+    cookiesale = randomCook();
+
+  }
+  // ****************** GLOBAL ************************
+  const arrOfHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+
+
+  // ******************* OBJECT LITERALS ***********************
+  const Seattle = {
+    name: Seattle,
+    minCust: 23,
+    maxCust: 65,
+    avgCookSale: 6.3,
+    hourCookSale: [],
+    numOfCust: randomCust(23, 65),
+  };
+// ***************** EXECUTABLE CODE ********************
